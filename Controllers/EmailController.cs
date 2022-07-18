5f4d205e2b48fed9c0404d90ec067b70e9f342bd
Sallal Haider <87648696+Sidhugee@users.noreply.github.com>
2022-07-18 12:31:46 +0500

    Add files via upload

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailServiceBlock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+         private readonly IEmailService _emailService;
+
+        public EmailController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(EmailDto request)
+        {
+            _emailService.SendEmail(request);
+            return Ok();
+        }
+    }
+}
---
+++ Services/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using EmailServiceBlock.Model;
+
+namespace EmailServiceBlock.Services
+{
+    public interface IEmailService
+    {
+        void SendEmail(EmailDto request);
+    }
+}
